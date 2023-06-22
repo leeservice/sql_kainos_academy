7cfccbe1821f144ad7350afd0f72a2edb8d41f1d
Patrick-Mc-Grath <patrick.mcgrath@kainos.com>
2023-06-22 13:21:10 +0100

    Create deliveryEmployee Table

--- employeeDB.sql
+++ employeeDB.sql
@@ -1,3 +1,11 @@
 CREATE DATABASE Kainoos_Softworn;
 
 USE Kainoos_Softworn;
+
+CREATE TABLE deliveryEmployees(
+deliveryEmployeeID smallint PRIMARY KEY AUTO_INCREMENT,
+fullName varchar(50) NOT NULL,
+salary decimal(10,2) NOT NULL,
+bankAccountNumber varchar(34) NOT NULL,
+nationalInsuranceNumber char(11)
+);
