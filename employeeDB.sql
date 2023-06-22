f6c0eed890191995518f51dbf1a309becc7d8598
stuferg <stuart.ferguson@kainos.com>
2023-06-22 13:50:26 +0100

    adding tables

--- employeeDB.sql
+++ employeeDB.sql
@@ -1,3 +1,29 @@
-CREATE DATABASE Kainoos_Softworn;
 
-USE Kainoos_Softworn;
+CREATE table sales_Employee (
+	salesEmployeeID smallint auto_increment,
+	fullName varchar(255) NOT NULL,
+    salary Decimal NOT NULL,
+    bankAccountNumber varchar(34) NOT NULL,
+    CREATE table sales_Employee (
+	salesEmployeeID smallint auto_increment,
+	fullName varchar(255) NOT NULL,
+    salary Decimal NOT NULL,
+    bankAccountNumber varchar(34) NOT NULL,
+    nationalInsuranceNumber char(11) NOT NULL,
+    commissionRate decimal NOT NULL,
+    commissionYTD decimal NOT NULL,
+    primary key(salesEmployeeID));
+    
+CREATE TABLE client (
+	clientID smallint auto_increment,
+	fullName varchar(50) NOT NULL,
+    address varchar(50) NOT NULL,
+    phone varchar(20),
+    dateJoined date,
+    salesEmployeeID smallint,
+    primary key(clientID));
+    
+ALTER TABLE client 
+	ADD CONSTRAINT k_client_salaesemployeeid
+	FOREIGN KEY(salesEmployeeID)
+	REFERENCES sales_Employee (SalesEmployeeID);
