d70f0d9f6a343ed33edb46bde32defd7b8fdd75c
Patrick-Mc-Grath <patrick.mcgrath@kainos.com>
2023-06-22 13:55:47 +0100

    Added delivery_Employee and delivery_Employee_Project tables

--- employeeDB.sql
+++ employeeDB.sql
@@ -9,3 +9,13 @@ salary decimal(10,2) NOT NULL,
 bankAccountNumber varchar(34) NOT NULL,
 nationalInsuranceNumber char(11)
 );
+
+CREATE TABLE delivery_Employee_Project(
+deliveryEmployeeID smallint NOT NULL,
+projectID smallint NOT NULL,
+startDate date NOT NULL,
+endDate date,
+FOREIGN KEY(deliveryEmployeeID) REFERENCES delivery_Employee(deliveryEmployeeID),
+FOREIGN KEY(projectID) REFERENCES project(projectID),
+PRIMARY KEY(deliveryEmployeeID, projectID)
+);
