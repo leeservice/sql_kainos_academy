6746571fbdfe074bf27e5e0679cd7c3b48e5f13d
stuferg <stuart.ferguson@kainos.com>
2023-06-22 15:06:51 +0100

    adding data in script

--- adding_projects.sql
+++
@@ -1,5 +0,0 @@
--- creating projects
-INSERT INTO client(projectName, projectValue, delieveryEmployeeID, 
-clientID, startDate, techID, projectStatus)
-	VALUES ('Horrible GitSQL Project', 01.99, '2', 1, 22/06/2023, 2, 
-0);    
