ea30db8a0ab467906a30248621135b1cefd214b6
leeservice <lee.service@kainos.com>
2023-06-22 15:45:57 +0100

    Adding creation script

--- kainoos_sql_tables_creation_script.sql
+++ kainoos_sql_tables_creation_script.sql
@@ -1,13 +1,45 @@
+-- Create the DB
+CREATE DATABASE IF NOT EXISTS kainoos_sql_db_LeeS;
+use kainoos_sql_db_LeeS;
+
 DELIMITER $$
 DROP PROCEDURE IF EXISTS creation_transaction $$
 CREATE PROCEDURE creation_transaction()
 BEGIN
     START TRANSACTION;
 
-    -- Create the DB
-    CREATE DATABASE IF NOT EXISTS kainoos_sql_db;
-
     -- Add tables
+    create table technologies (
+	technologyID smallint primary key auto_increment,
+    `name` varchar(50) not null,
+    description varchar(200) not null);
+    
+   create table project (
+    projectID smallint primary key auto_increment,
+    projectName Varchar(50) not null, 
+    projectValue decimal(10,2) not null,
+    deliveryEmployeeProjectID smallint not null,
+    clientID smallint not null,
+    startDate date not null,
+    techID smallint not null,
+    projectStatus tinyint(1) not null
+   );
+
+    CREATE TABLE IF NOT EXISTS client (
+        clientID smallint auto_increment,
+        fullName varchar(50) NOT NULL,
+   	address varchar(50) NOT NULL,
+   	phone varchar(20),
+   	dateJoined date,
+   	salesEmployeeID smallint,
+   	primary key(clientID));
+
+    CREATE TABLE IF NOT EXISTS delivery_Employee(
+	deliveryEmployeeID smallint PRIMARY KEY AUTO_INCREMENT,
+	fullName varchar(50) NOT NULL,
+	salary decimal(10,2) NOT NULL,
+	bankAccountNumber varchar(34) NOT NULL,
+	nationalInsuranceNumber char(11));
 
 CREATE TABLE IF NOT EXISTS sales_Employee (
         salesEmployeeID smallint auto_increment,
@@ -19,26 +51,10 @@ CREATE TABLE IF NOT EXISTS sales_Employee (
    	commissionYTD decimal NOT NULL,
    	primary key(salesEmployeeID));
 
-CREATE TABLE IF NOT EXISTS client (
-        clientID smallint auto_increment,
-        fullName varchar(50) NOT NULL, 
-   	address varchar(50) NOT NULL,
-   	phone varchar(20),
-   	dateJoined date,
-   	salesEmployeeID smallint,
-   	primary key(clientID));
-    
 ALTER TABLE client
-        ADD CONSTRAINT k_client_salaesemployeeid
+        ADD CONSTRAINT fk_client_salaesemployeeid
         FOREIGN KEY(salesEmployeeID)
         REFERENCES sales_Employee (SalesEmployeeID);
-
-CREATE TABLE IF NOT EXISTS delivery_Employee(
-	deliveryEmployeeID smallint PRIMARY KEY AUTO_INCREMENT,
-	fullName varchar(50) NOT NULL,
-	salary decimal(10,2) NOT NULL,
-	bankAccountNumber varchar(34) NOT NULL,
-	nationalInsuranceNumber char(11));
 
 CREATE TABLE IF NOT EXISTS delivery_Employee_Project(
 	deliveryEmployeeID smallint NOT NULL,
@@ -49,20 +65,26 @@ CREATE TABLE IF NOT EXISTS delivery_Employee_Project(
 	CONSTRAINT FK_projectID FOREIGN KEY(projectID) REFERENCES project(projectID),
 	PRIMARY KEY(deliveryEmployeeID, projectID));
 
-CREATE TABLE IF NOT EXISTS technologies (
-	technologyID smallint primary key auto_increment,
-    	`name` varchar(50) not null,
-    	description varchar(200) not null);
-    
 CREATE TABLE IF NOT EXISTS technologies_Project (
 	technologiesProjectID smallint primary key auto_increment,
     	technologyID smallint,
-    	foreign key (technologyID) references technologies(techologyID),
-    	projectID smallint,
-    	foreign key (projectID) references project(projectID));
-   
+    	projectID smallint);
+
+ALTER TABLE technologies_Project
+    add constraint fk_technologyID_technologies
+    foreign key (technologyID) 
+    references technologies(technologyID);
+
+ALTER TABLE technologies_Project
+    add constraint fk_projectID_project
+    foreign key (projectID) 
+    references project(projectID);
+
+ALTER TABLE project
+    add constraint fk_techID_technologiesProject
+    foreign key (techID) 
+    references technologies_Project(projectID);
 
 END $$
 DELIMITER ;
 CALL creation_transaction();
-        
