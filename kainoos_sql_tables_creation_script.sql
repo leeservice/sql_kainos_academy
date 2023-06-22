2f9c13ef120a5b1b633d6ba7e25a6a16f67abbc6
stuferg <stuart.ferguson@kainos.com>
2023-06-22 14:17:07 +0100

    Adding stuarts tables

--- kainoos_sql_tables_creation_script.sql
+++ kainoos_sql_tables_creation_script.sql
@@ -8,6 +8,31 @@ BEGIN
     create database kainoos_sql_db;
 
     -- Add tables
+
+CREATE table sales_Employee (
+        salesEmployeeID smallint auto_increment,
+        fullName varchar(255) NOT NULL,
+    salary Decimal NOT NULL,
+    bankAccountNumber varchar(34) NOT NULL,
+    nationalInsuranceNumber char(11) NOT NULL,
+    commissionRate decimal NOT NULL,
+    commissionYTD decimal NOT NULL,
+    primary key(salesEmployeeID));
+
+CREATE TABLE client (
+        clientID smallint auto_increment,
+        fullName varchar(50) NOT NULL, 
+    address varchar(50) NOT NULL,
+    phone varchar(20),
+    dateJoined date,
+    salesEmployeeID smallint,
+    primary key(clientID));
+    
+ALTER TABLE client
+        ADD CONSTRAINT k_client_salaesemployeeid
+        FOREIGN KEY(salesEmployeeID)
+        REFERENCES sales_Employee (SalesEmployeeID);
+
     create table technologies (
 	technologyID smallint primary key auto_increment,
     `name` varchar(50) not null,
