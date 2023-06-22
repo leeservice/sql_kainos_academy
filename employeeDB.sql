248e6d5e576d7c2538bc3f4387d2fb6983a4ed4a
Patrick-Mc-Grath <patrick.mcgrath@kainos.com>
2023-06-22 13:28:43 +0100

    Fixed inconsistency

--- employeeDB.sql
+++ employeeDB.sql
@@ -2,7 +2,7 @@ CREATE DATABASE Kainoos_Softworn;
 
 USE Kainoos_Softworn;
 
-CREATE TABLE deliveryEmployees(
+CREATE TABLE delivery_Employee(
 deliveryEmployeeID smallint PRIMARY KEY AUTO_INCREMENT,
 fullName varchar(50) NOT NULL,
 salary decimal(10,2) NOT NULL,
