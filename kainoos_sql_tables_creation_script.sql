f0ad968bc2481e4ece96999016281769ebcdeb94
Patrick-Mc-Grath <patrick.mcgrath@kainos.com>
2023-06-22 14:20:22 +0100

    added patrick's tables

--- kainoos_sql_tables_creation_script.sql
+++ kainoos_sql_tables_creation_script.sql
@@ -33,6 +33,24 @@ ALTER TABLE client
         FOREIGN KEY(salesEmployeeID)
         REFERENCES sales_Employee (SalesEmployeeID);
 
+CREATE TABLE IF NOT EXISTS delivery_Employee(
+deliveryEmployeeID smallint PRIMARY KEY AUTO_INCREMENT,
+fullName varchar(50) NOT NULL,
+salary decimal(10,2) NOT NULL,
+bankAccountNumber varchar(34) NOT NULL,
+nationalInsuranceNumber char(11)
+);
+
+CREATE TABLE IF NOT EXISTS delivery_Employee_Project(
+deliveryEmployeeID smallint NOT NULL,
+projectID smallint NOT NULL,
+startDate date NOT NULL,
+endDate date,
+CONSTRAINT FK_delEmpID FOREIGN KEY(deliveryEmployeeID) REFERENCES delivery_Employee(deliveryEmployeeID),
+CONSTRAINT FK_projectID FOREIGN KEY(projectID) REFERENCES project(projectID),
+PRIMARY KEY(deliveryEmployeeID, projectID)
+);
+
     create table technologies (
 	technologyID smallint primary key auto_increment,
     `name` varchar(50) not null,
